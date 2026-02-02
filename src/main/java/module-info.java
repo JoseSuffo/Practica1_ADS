44a7d572e66aa725ef336d245cb344195d3d4a73
José Ramón Suffo Peimbert <suffo.jose@uabc.edu.mx>
2026-02-02 14:09:40 -0800

    Cambios en archivos pom.xml y module-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,11 @@ module com.example.practica1_ads {
     requires javafx.fxml;
     requires javafx.web;
 
+    // Conectividad de Base de Datos
+    requires java.sql;
+    requires org.mariadb.jdbc;
+
+    // UI Libraries
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
@@ -11,6 +16,11 @@ module com.example.practica1_ads {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
+    // Paquete principal de JavaFX
     opens com.example.practica1_ads to javafx.fxml;
     exports com.example.practica1_ads;
+
+    // IMPORTANTE: Exportar el paquete donde está tu lógica de DB
+    // (Asegúrate de que el nombre coincida exactamente con 'package Clases;' en tu archivo .java)
+    exports Clases;
 }
